a897f381d500dcaa09d4ef7794cb1b43be6b1f8d
allonsky <alexander.krasnikov1@gmail.com>
2019-11-02 16:31:54 +0300

    first project

---
+++ сишурп1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace сишурп1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, y = 0, g;
+            for (double v = 0 ; v <= 3; v++)
+            {
+                x += v;
+                y++;
+            }
+            g = x / y;
+            Console.WriteLine(g);
+            Console.ReadKey();
+        }
+    }
+}
